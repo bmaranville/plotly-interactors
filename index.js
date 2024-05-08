d2f675453ac61b9a1b1211ff505e7cf367395599
Brian Benjamin Maranville <brian.maranville@nist.gov>
2024-05-08 16:26:53 -0400

    add ellipseInteractor to exports

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export { angleSliceInteractor } from './src/angle-slice-interactor.js';
 export { xSliceInteractor } from './src/x-slice-interactor.js';
+export { ellipseInteractor } from './src/ellipse-interactor.js';

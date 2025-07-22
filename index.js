cc33091ea349dd24a4855c439b12585362962865
bbm <brian.maranville@nist.gov>
2025-07-22 12:26:10 -0400

    add vertical line interactor

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export { angleSliceInteractor } from './src/angle-slice-interactor.js';
 export { xSliceInteractor } from './src/x-slice-interactor.js';
+export { xLineInteractor } from './src/x-line-interactor.js';
 export { ellipseInteractor } from './src/ellipse-interactor.js';
 export { getInteractorLayer } from './src/interactor-layer.js';
